4ea5a8447ca5a5ec10c59e0dedf236617ec203bf
valeriyProg <val.fowork@gmail.com>
2019-12-15 17:21:28 +0200

    update pack

--- src/common/services/package.service.ts
+++ src/common/services/package.service.ts
@@ -5,7 +5,7 @@ import Package from '../contracts/package';
   providedIn: 'root'
 })
 export class PackageService {
-  packageList  = [
+  packageList: Package[]  = [
     new Package(
       'PL0001',
       '',
@@ -38,41 +38,26 @@ export class PackageService {
       'P0001',
       '',
       'box',
-      'Box XY',
+      'Package 1',
       '',
-      'Some description',
-      '0.49',
-      '0.26',
-      '1.8',
-      '3.56',
+      'Empty box',
+      '0.7',
+      '1',
+      '5',
+      '30',
       '',
       ''),
   ];
   constructor() { }
+  add(pack: Package): void {
+    this.packageList.push(pack);
+  }
+  remove(pack: Package): void {
+    try {
+      const index = this.packageList.indexOf(pack);
+      this.packageList.splice(index, 1);
+    } catch  {
+      console.log('Item not found');
+    }
+  }
 }
-// {
-//   client: clientItemsData[1],
-//     destinationPoint: postItemsData[0][1],
-//   startingPoint: postItemsData[1][0],
-//   type: 'letter',
-//   weight: '0.1',
-//   height: '0,15',
-//   width: '0.30',
-//   price: '30',
-//   description: 'Very important letter for my dog!',
-//   name: 'MyLetter1',
-//   screen: 'email'
-// },
-// {
-//   client: clientItemsData[1],
-//     destinationPoint: postItemsData[1][0],
-//   startingPoint: postItemsData[0][1],
-//   type: 'package',
-//   weight: '5',
-//   height: '0.7',
-//   width: '1',
-//   price: '150',
-//   description: 'Empty box',
-//   name: 'Package 1',
-//   screen: 'local_mall'
-// }
