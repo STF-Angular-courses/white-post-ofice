ba03dbe7630a382063c91f88a9d0e0a9745c138a
valeriyProg <val.fowork@gmail.com>
2019-12-18 09:26:25 +0200

    add fix project

---
+++ src/app/app.component.ts
@@ -0,0 +1,68 @@
+import {Component, EventEmitter, OnInit} from '@angular/core';
+import {PostService} from '../common/services/post.service';
+import {ClientService} from '../common/services/client.service';
+import {PostCompany} from '../common/contracts/post-company.contract';
+import PostOffice from '../common/contracts/post-office.contract';
+import {Client} from '../common/models/client.model';
+import {PostOfficeService} from '../common/services/post-office.service';
+import {Package} from '../common/models/package.model';
+import {PackageService} from '../common/services/package.service';
+import Organisation from '../common/contracts/abstract-organisation.contract';
+import {ItCompany} from '../common/contracts/it-company.contract';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  postCompanyList: PostCompany[];
+  postDepartmentList: PostOffice[];
+  clientsList: Client[];
+  packageList: Package[];
+  showPostList = true;
+  showClientList = false;
+  showPackageList = false;
+  showPostOfficesList = false;
+  itemToDetail: PostCompany | PostOffice | Client | Package ;
+  constructor(
+    private postService: PostService,
+    private postOfficeService: PostOfficeService,
+    private clientService: ClientService,
+    private packageService: PackageService
+  ) { }
+  ngOnInit(): void {
+    this.postCompanyList = this.postService.postCompanyList;
+    this.postDepartmentList = this.postOfficeService.postOfficesList;
+    this.clientsList = this.clientService.clientList;
+    this.packageList = this.packageService.packageList;
+  }
+  postCompanyDetail(event: PostCompany ) {
+    this.hideAllList();
+    this.itemToDetail = event;
+  }
+  // TODO: NOT USING IN THIS MOMENT
+  // postOfficeDetail(event: PostOffice) {
+  //   this.hideAllList();
+  //   this.itemToDetail = event;
+  // }
+  // clientDetail(event: Client) {
+  //   this.hideAllList();
+  //   this.itemToDetail = event;
+  // }
+  // packageDetail(event: Package) {
+  //   this.hideAllList();
+  //   this.itemToDetail = event;
+  // }
+  //
+  hideAllList() {
+    this.showPostList = false;
+    this.showClientList = false;
+    this.showPackageList = false;
+    this.showPostOfficesList = false;
+    this.itemToDetail = undefined;
+  }
+}
+
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatIconModule, MatListModule} from '@angular/material';
+import { ClientListComponent } from './post-clients/client-list/client-list.component';
+import { PackageListComponent } from './post-packages/package-list/package-list.component';
+import { PostListComponent } from './post-structs/post-list/post-list.component';
+import {PostItemComponent} from './post-structs/post-company/post-item.component';
+import {ClientItemComponent} from './post-clients/client-item/client-item.component';
+import {PackageItemComponent} from './post-packages/package-item/package-item.component';
+import {OfficesListComponent} from './post-structs/offices-list/offices-list.component';
+import {PostOfficeComponent} from './post-structs/post-office/post-office.component';
+import {PostOfficeService} from '../common/services/post-office.service';
+import {PostService} from '../common/services/post.service';
+import {ClientService} from '../common/services/client.service';
+import {PackageService} from '../common/services/package.service';
+import { PostCompanyDetailComponent } from './post-company-detail/post-company-detail.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostItemComponent,
+    PostOfficeComponent,
+    ClientItemComponent,
+    PackageItemComponent,
+    ClientListComponent,
+    PackageListComponent,
+    PostListComponent,
+    OfficesListComponent,
+    PostCompanyDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+  ],
+  providers: [PostOfficeService, PostService, ClientService, PackageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
