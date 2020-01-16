7286962cfa3ee60ced86e710853dad80941625db
valeriyProg <val.fowork@gmail.com>
2020-01-16 16:21:37 +0200

    some fix 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {PostCompany} from './post-company/common/contracts/post-company.contract';
-import PostDepartment from './post-department/common/contracts/post-department.contract';
-import Client from './client/common/contracts/client.contract';
-import Package from './package/common/contracts/package';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  itemToDetail: PostCompany | PostDepartment | Client | Package ;
-}
+export class AppComponent { }
 
 
 
