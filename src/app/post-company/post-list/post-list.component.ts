4a55c4ed4fca964ca73bb45c7028165536d56bbc
valeriyProg <val.fowork@gmail.com>
2020-01-16 17:02:11 +0200

    some fix 3

--- src/app/post-company/post-list/post-list.component.ts
+++ src/app/post-company/post-list/post-list.component.ts
@@ -8,14 +8,11 @@ import {PostCompany} from '../common/contracts/post-company.contract';
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
-  @Output() postCompanyDetail = new EventEmitter<PostCompany>();
   postCompanyList: PostCompany[];
   constructor(private postService: PostService) { }
 
   ngOnInit() {
     this.postCompanyList = this.postService.postCompanyList;
   }
-  showDetail(item: PostCompany) {
-    this.postCompanyDetail.emit(item);
-  }
+
 }
